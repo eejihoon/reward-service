811a0159381acefaeaa537ce16661b197c2b782f
cocodori <moradulee@gmail.com>
2022-11-25 00:28:19 +0900

    #1 Create CouponEvent, Coupon Entity, Repository

---
+++ coupon-core/src/main/kotlin/com/coupon/core/domain/Coupon.kt
@@ -0,0 +1,24 @@
+package com.coupon.core.domain
+
+import java.time.LocalDate
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+internal class Coupon(
+    publishedAt: LocalDate,
+    expiredAt: LocalDateTime,
+    couponEvent: CouponEvent,
+): BaseEntity() {
+
+    val publishedAt: LocalDate = publishedAt
+
+    val expiredAt: LocalDateTime = expiredAt
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "couponEvent_id", unique = false, nullable = false)
+    val couponEvent = couponEvent
+
+    var memberId: Long? = null
+}
+
